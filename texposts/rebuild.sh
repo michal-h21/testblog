736ffadd935fda25de48b0421ecc08f72cc8c02d
Michal Hoftich <michal.h21@gmail.com>
2021-07-25 23:41:46 +0200

    use cd

--- texposts/rebuild.sh
+++ texposts/rebuild.sh
@@ -12,13 +12,13 @@ do
   texdir=`echo $i | cut -d@ -f1 -`
   texfile=`echo $i | cut -d@ -f2 -`
   # either execute Makefile, or run make4ht directly
-  pushd "$texdir"
+  cd "$texdir"
   if test -f Makefile; then
     make
   else
     make4ht -m publish -l "$texfile"
   fi
-  popd
+  cd ..
   # echo pokus $pokus
   # echo world $world
 done
